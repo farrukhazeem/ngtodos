e5157eef93682850c28fa46094816ff2e7e3c0d1
smshahrukh <shahrukh.2790@gmail.com>
2017-10-14 00:00:11 +0500

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+//import { AngularFire, AuthProviders, AuthMethods, FirebaseListObservable } from 'angularfire2';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs/Observable';
+import * as firebase from 'firebase/app';
+import { AngularFireDatabaseModule, AngularFireDatabase, AngularFireList } from 'angularfire2/database';
+import { AngularFireAuthModule, AngularFireAuth } from 'angularfire2/auth';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +12,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  todosRef: AngularFireList<any>;
+  currentTodo: FormGroup;
+  todos: Observable<any[]>;
+  editTodos = { title: '', description: '' };
+  editMode = false;
+  currentTodoKey;
+
+  constructor(private _FormBuilder: FormBuilder, private db: AngularFireDatabase) {
+    this.todosRef = db.list('todos');
+    this.currentTodo = _FormBuilder.group({
+      'title': [null, Validators.compose([Validators.required, Validators.minLength(5), Validators.maxLength(50)])],
+      'description': [null, Validators.compose([Validators.required, Validators.minLength(5), Validators.maxLength(200)])]
+    });
+    this.todos = this.todosRef.snapshotChanges().map(changes => {
+        return changes.map(c => ({ key: c.payload.key, ...c.payload.val() }));
+    })
+   // db.list('/todos').snapshotChanges();
+  }
+
+ addTodo(todo) {
+  this.db.list('/todos').push(todo);
+ 
+ //  addTodo(title: String) {
+    // this.todosRef.push({text: title});
+
+}
+
+  deleteTodo(key: string) {
+    // console.warn(todo)
+    // const promise = this.db.object(`todos/${todo.key}`).remove();
+    // promise.then(_ => console.log('success'))
+    //   .catch(err => console.log(err, 'You dont have access!'));
+      this.todosRef.remove(key);
+  }
+
+  editTodo(todo){
+      this.todos.forEach((todo) => {
+        console.log(todo);
+      })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AngularFireModule } from 'angularfire2';
+import { HttpModule } from '@angular/http';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
+import { AngularFireDatabase  } from 'angularfire2/database';
 
+export const firebaseConfig = {
+  apiKey: "AIzaSyCReWg92UdiVZYTKL7N6GiMSqcrZ7q4yyo",
+  authDomain: "ngtodo-d3811.firebaseapp.com",
+  databaseURL: "https://ngtodo-d3811.firebaseio.com",
+  projectId: "ngtodo-d3811",
+  storageBucket: "ngtodo-d3811.appspot.com",
+  messagingSenderId: "501928283258"
+};
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
-  providers: [],
+  providers: [AngularFireDatabase],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
